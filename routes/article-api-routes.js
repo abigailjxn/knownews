d2cc11ae3879adcf3f7a7966dcb68b33ac124b7d
Abigail Jackson <41799687+abigailjxn@users.noreply.github.com>
2019-01-22 23:40:41 -0600

    working GET route for /article/:id

--- routes/article-api-routes.js
+++ routes/article-api-routes.js
@@ -1,50 +1,61 @@
 const db = require("../models");
 
-module.exports = function(app, axios, cheerio) {
-    
-    app.get("/scrape", function(req, res) {
+module.exports = function (app, axios, cheerio) {
+    /////SCRAPE ROUTE/////
+    app.get("/scrape", function (req, res) {
         axios.get("https://mashable.com/category/tech/")
-        .then(function(response){
-            const $ = cheerio.load(response.data);
+            .then(function (response) {
+                const $ = cheerio.load(response.data);
 
-            $("article h2").each(function(i, element){
-                const result = {};
+                $("article h2").each(function (i, element) {
+                    const result = {};
 
-                result.title = $(this)
-                .children("a")
-                .text();
-                result.link = $(this)
-                .children("a")
-                .attr("href");
-                // result.img = $(this)
-                // .children(".article-img-container")
-                // .children("a")
-                // .chilren("img")
-                // .attr("src");
-                // result.excerpt = $(this)
-                // .children(".article-excerpt")
-                // .text();
+                    result.title = $(this)
+                        .children("a")
+                        .text();
+                    result.link = $(this)
+                        .children("a")
+                        .attr("href");
+                    // result.img = $(this)
+                    // .children(".article-img-container")
+                    // .children("a")
+                    // .chilren("img")
+                    // .attr("src");
+                    // result.excerpt = $(this)
+                    // .children(".article-excerpt")
+                    // .text();
 
-                db.Article.create(result)
-                .then(function(dbArticle){
-                    console.log(dbArticle);
-                })
-                .catch(function(err){
-                    console.log(err);
-                })
-               
-            });
-            res.send("scrape complete!");
-        })
+                    db.Article.create(result)
+                        .then(function (dbArticle) {
+                            console.log(dbArticle);
+                        })
+                        .catch(function (err) {
+                            console.log(err);
+                        })
+
+                });
+                res.send("scrape complete!");
+            })
     });
-
-    app.get("/articles", function(req, res){
+    /////GET ARTICLES IN MONGODB/////
+    app.get("/articles", function (req, res) {
         db.Article.find({})
+            .then(function (dbArticle) {
+                res.json(dbArticle)
+            })
+            .catch(function (err) {
+                res.json(err)
+            });
+    });
+/////GET ARTICLE WITH COMMENTS/////
+    app.get("/articles/:id", function(req, res){
+        db.Article.findOne({_id: req.params.id})
+        .populate("comment")
         .then(function(dbArticle){
-            res.json(dbArticle)
+            res.json(dbArticle);
         })
         .catch(function(err){
-            res.json(err)
+            res.json(err);
         });
-    })
+    });
 }
