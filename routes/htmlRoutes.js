e08a7d6fbef9bf4317251322bf7e36b3fac475ae
Abigail Jackson <41799687+abigailjxn@users.noreply.github.com>
2019-01-23 19:23:15 -0600

    working home page that renders articles

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,9 +3,17 @@ const db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-      res.render("index", {
-        msg: "Welcome!"
-      });
+      db.Article.find({})
+      .then(function (dbArticle) {
+        res.render("index", {
+            msg: "Welcome!",
+            articleData: dbArticle
+          });
+    })
+    .catch(function (err) {
+        res.json(err)
+    });
+      
    
   });
 
