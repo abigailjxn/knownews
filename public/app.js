1a03dd4458c05b8717da6dda4d90342379849123
Abigail Jackson <41799687+abigailjxn@users.noreply.github.com>
2019-01-25 01:01:04 -0600

    POST route responding for Comment, but not creating new collection in MongoDB

--- public/app.js
+++ public/app.js
@@ -12,4 +12,23 @@ $("#getarticles").on("click", function(event){
 $(".comment-btn").on("click", function(event){
     event.preventDefault();
     UIkit.modal("#comment-modal").show();
-})
+    const thisId = $(this).attr("id");
+    console.log(thisId);
+
+    $("#comment-confirm").on("click", function(event){
+        const commentInfo = {
+            name: $("#comment-name").val().trim(),
+            body: $("#comment-body").val().trim()
+        };
+        console.log(commentInfo);
+        $.ajax({
+            type: "POST",
+            url: "/api/articles/" + thisId,
+            data: commentInfo
+        })
+        .then(function(data){
+            console.log(data);
+        });
+    });
+});
+
